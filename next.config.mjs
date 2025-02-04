036a9322f83563028c7ca88114560dbc62d16867
dveretenov1 <dveretenov@gmail.com>
2025-02-04 01:35:04 -0500

    dev

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:5000/:path*',
+        destination: 'http://api:5000/:path*',
       },
     ];
   },
