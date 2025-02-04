7a69368be8558b9d0805ce9d352bd6a6ba5e3445
dveretenov1 <dveretenov@gmail.com>
2025-02-03 20:03:26 -0500

    Clean up repository structure

--- api/app/schemas/__init__.py
+++
@@ -1,4 +0,0 @@
-from .user import User, UserCreate, UserBase
-from .file import File, FileCreate, FileBase
-from .chat import Chat, ChatCreate, ChatBase
-from .message import Message, MessageCreate, MessageBase
